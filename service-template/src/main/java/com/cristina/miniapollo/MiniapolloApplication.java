fc9c0ac350e486dde44891e720b32fa8735e44f0
Cristina Junca <cristina-sirbu>
2025-06-10 10:35:11 +0200

    Added service template in its incipient form

---
+++ service-template/src/main/java/com/cristina/miniapollo/MiniapolloApplication.java
@@ -0,0 +1,13 @@
+package com.cristina.miniapollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniapolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniapolloApplication.class, args);
+	}
+
+}
---
+++ service-template/src/test/java/com/cristina/miniapollo/MiniapolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristina.miniapollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniapolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
